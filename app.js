8ee765d8e77031ccbcf605fea04e82e1437204b2
Andres Molina <andres.molinae29@gmail.com>
2022-08-08 19:25:49 -0500

    cambios en la rama carrito

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/cart.html'));
+});
+
 app.listen(3001, () => {
     console.log('Servidor corriendo en el puerto 3001');
 });
