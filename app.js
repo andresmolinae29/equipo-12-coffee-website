ca5115a8f69d5ddddfdd28dba9e29f7efc7ad4b3
Julian Mcc <julianmaccio@gmail.com>
2022-08-15 14:16:12 -0300

    cambios estilo commit p/pull f/carrito

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 
 app.use( express.static(publicPath) );  
