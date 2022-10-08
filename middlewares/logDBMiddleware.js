cc9112e104aa0fe438f13ff0f2ad613449c4ebd5
Andres Molina <andres.molinae29@gmail.com>
2022-10-08 13:21:10 -0500

    Initial middleware

--- middlewares/logDBMiddleware.js
+++ middlewares/logDBMiddleware.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 function logDBMiddleware(req, res, next) {
-    fs.appendFileSync('logDB.txt', 'Se ingreso en la pagina' + req.url);
+    fs.appendFileSync('logDB.txt', 'Se creo un registro al ingresar a ' + req.url);
+
+    // Falta la logica
     next();
 }
 
